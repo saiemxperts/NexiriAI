0ea36ec883aac8fcd4f14dcaedb33d7686f9f56d
saiemxperts <102948896+saiemxperts@users.noreply.github.com>
2026-07-23 18:56:33 +0500

    /

--- nexiri-ai/next.config.ts
+++ nexiri-ai/next.config.ts
@@ -1,6 +1,13 @@
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
